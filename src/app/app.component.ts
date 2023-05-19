a009cedea5d9d30bb961997249fda5efdba2071e
Micah Dittmar <Micah.dittmar@gmail.com>
2023-05-19 16:25:45 -0400

    Added changes to home page and style differences

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private router: Router) {}
   title = 'Micah Dittmar';
+
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  navigateTo(url: string): void {
+    // Call the scroll function first
+    this.scrollToTop();
+    
+    // Use router.navigate to navigate to the desired URL
+    this.router.navigate([url]);
+  }
 }
